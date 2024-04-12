db06807c37a7fd384e74865db2656e6ae0f6b51e
om-abhiwan <om.abhiwan@gmail.com>
2024-04-12 16:21:11 +0530

    For non response error solved

--- controllers/gettingAnswers.js
+++ controllers/gettingAnswers.js
@@ -10,21 +10,23 @@ exports.getAnswerTest = async (req, res) => {
 
         
         
-        const media = await UploadMedia.find({question_id:resp._id.toString()}).select({ categories: 1, link: 1, _id: 0 })
-
-        if(media.length!=0){
-            data = media
+        
+        let noResult = "I'm sorry, I didn't understand that."
+        
+        
+        if(resp==="I'm sorry, I didn't understand that."){
+            return res.status(200).json({response:noResult,data:data})
+        }else{
+            console.log("Hii")
+            const media = await UploadMedia.find({question_id:resp._id.toString()}).select({ categories: 1, link: 1, _id: 0 })
+            if(media.length!=0){
+                data = media
+            }
         }
         
         console.log(media)
         
         
-        let noResult = "I'm sorry, I didn't understand that."
-
-
-        if(resp==="I'm sorry, I didn't understand that."){
-            return res.status(200).json({response:noResult,data:data})
-        }
 
 
 
