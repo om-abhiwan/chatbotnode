2d15073f5fd202e41470563481a88b9376e3483b
om-abhiwan <om.abhiwan@gmail.com>
2024-04-12 10:58:19 +0530

    Test

--- Routes/answersRoutes.js
+++ Routes/answersRoutes.js
@@ -5,6 +5,8 @@ const answerController = require("../controllers/gettingAnswers")
 
 // answerRoutes.post("/answer",answerController.getAnswer)
 answerRoutes.post("/answer",answerController.getAnswerTest)
-
+answerRoutes.get("/",(req,res)=>{
+    res.send("Hello")
+})
 
 module.exports  = answerRoutes
