b1c763f0e98d14f7f9385b3295a6c14c17a7630c
om-abhiwan <om.abhiwan@gmail.com>
2024-04-15 10:31:19 +0530

    Routes Updated

--- Routes/answersRoutes.js
+++ Routes/answersRoutes.js
@@ -3,10 +3,10 @@ const answerRoutes = express.Router()
 const answerController = require("../controllers/gettingAnswers")
 
 
-// answerRoutes.post("/answer",answerController.getAnswer)
 // answerRoutes.post("/answer",answerController.getAnswerTest)
 
 
+answerRoutes.post("/answer",answerController.getAns)
 // answerRoutes.post("/ans",answerController.getAns)
 
 
